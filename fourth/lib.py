0aafa06486597d63e16e82d001b636ad9be5f18f
assisken <assisken@gmail.com>
2019-03-25 01:11:57 +0300

    Working on easy iter method...

--- fourth/lib.py
+++ fourth/lib.py
@@ -1,7 +1,7 @@
 import itertools
-import numpy as np
 
-from sympy import diff, symbols, Matrix, Symbol, lambdify
+import numpy as np
+from sympy import diff, symbols, Matrix, Symbol
 
 
 def get_yakobi(F: Matrix, _symbols: str) -> Matrix:
@@ -27,9 +27,42 @@ def newton_solve(sym, start, F, e):
         X = Matrix([[x_old], [y_old]])
         res: Matrix = X - W.subs({x_s: x_old, y_s: y_old}) * F.subs({x_s: x_old, y_s: y_old})
         x, y = map(lambda x: float(x), itertools.chain.from_iterable(res.tolist()))
+
         x_res.append(x)
         y_res.append(y)
         if e > np.sqrt((x - x_old) ** 2 + (y - y_old) ** 2):
             break
         x_old, y_old = x, y
+    return x_res, y_res, count
+
+
+def easy_iter(F, x, symbol, e):
+    r = 0.1
+    x_s = symbols(symbol)
+    primal = diff(F(x_s), x_s)
+    res = primal.subs({symbol: x})
+    print(res)
+    if abs(e - r * res) >= 1:
+        raise Exception(f'{res} не сходится.')
+    if res > 0:
+        return r
+    else:
+        return -r
+
+
+def iter_solve(start, F, e):
+    count = 0
+    x_old, y_old = start
+    x_res, y_res = [x_old], [y_old]
+
+    while True:
+        y = float(y_old - easy_iter(F[0], y_old, 'y', e) * F[0](y_old))
+        x = float(x_old - easy_iter(F[1], x_old, 'x', e) * F[1](x_old))
+
+        x_res.append(x)
+        y_res.append(y)
+        if e > np.sqrt((x - x_old) ** 2 + (y - y_old) ** 2):
+            break
+        x_old, y_old = x, y
+
     return x_res, y_res, count
--- fourth/run.py
+++ fourth/run.py
@@ -1,10 +1,10 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from numpy.ma import arctan
-from sympy import symbols, Matrix
+from sympy import symbols, Matrix, atan, sqrt
 from sympy import tan
 
-from fourth.lib import newton_solve
+from fourth.lib import newton_solve, iter_solve
 
 plt.grid(True)
 plt.axhline(0, color='black')
@@ -15,6 +15,7 @@ plt.xlim(-1.5, 1.5)
 _f1 = lambda x, y: tan(x * y + 0.1) - x ** 2
 f1 = lambda x: (arctan(x**2) - 0.1) / x
 _f2 = lambda x, y: 0.6 * x ** 2 + 2 * y ** 2 - 1
+f2 = lambda y: np.sqrt(5 / 3 - 10 / 3 * y ** 2)
 
 x = np.linspace(-1.5, 1.5, 101)
 y = np.linspace(-1.5, 1.5, 101)
@@ -23,26 +24,44 @@ F = _f2(X, Y)
 plt.contour(X, Y, F, [0])
 plt.plot(x, f1(x))
 
-x_s, y_s = symbols('x y')
-F = Matrix([
-    [_f1(x_s, y_s)],
-    [_f2(x_s, y_s)]
-])
+# x_s, y_s = symbols('x y')
+# F = Matrix([
+#     [_f1(x_s, y_s)],
+#     [_f2(x_s, y_s)]
+# ])
 
 e = 0.0001
 starts = [
     (1.0, 0.5),
-    (0.0, -0.5),
-    (-1.0, -0.5),
-    (-0.0, 0.5),
+    # (0.0, -0.5),
+    # (-1.0, -0.5),
+    # (-0.0, 0.5),
 ]
-print('Метод Ньютона:')
+
+# print('Метод Ньютона:')
+# for start in starts:
+#     x0, y0 = start
+#     x_res, y_res, count = newton_solve('x y', (x0, y0), F, e)
+#     print(f'\tДля точки {start}:')
+#     print(f'\tКоличество итераций: {count}')
+#     print(f'\t\tx = {x_res[-1]}; y = {y_res[-1]}')
+#
+#     plt.plot(x_res, y_res, 'ro-', color='#ff0000')
+# plt.savefig('fig1.png', dpi=150)
+
+
+f1 = lambda x: (atan(x**2) - 0.1) / x   # y = ...
+f2 = lambda y: sqrt(5/3 - 10/3 * y**2)  # x = ...
+F = [f1, f2]
+print('Метод простых итераций:')
 for start in starts:
-    x0, y0 = start
-    x_res, y_res, count = newton_solve('x y', (x0, y0), F, e)
+    x_res, y_res, count = iter_solve(start, F, e)
     print(f'\tДля точки {start}:')
     print(f'\tКоличество итераций: {count}')
     print(f'\t\tx = {x_res[-1]}; y = {y_res[-1]}')
 
     plt.plot(x_res, y_res, 'ro-', color='#ff0000')
-plt.savefig('fig1.png', dpi=150)
+
+
+plt.savefig('fig2.png', dpi=150)
+# plt.show()
