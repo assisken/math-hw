7fbebb9429909dc09882966d6cf2d3fcc388030c
assisken <assisken@gmail.com>
2019-03-24 23:43:15 +0300

    Newton method does work. Finally.

--- fourth/lib.py
+++ fourth/lib.py
@@ -1,10 +1,35 @@
+import itertools
 import numpy as np
-from sympy import diff, symbols
+
+from sympy import diff, symbols, Matrix, Symbol, lambdify
 
 
-def get_yakobi(functions: list, _symbols: str):
+def get_yakobi(F: Matrix, _symbols: str) -> Matrix:
     sym = symbols(_symbols)
+    if isinstance(sym, (str, Symbol)):
+        sym = [sym]
     diffs = []
+    functions = list(itertools.chain.from_iterable(F.tolist()))
     for func in functions:
         diffs.append([diff(func, s) for s in sym])
-    return np.matrix(diffs)
+    return Matrix(diffs)
+
+
+def newton_solve(sym, start, F, e):
+    x_s, y_s = symbols(sym)
+    count = 0
+    W = get_yakobi(F, 'x y').inv()
+    x_old, y_old = start
+    x_res, y_res = [x_old], [y_old]
+
+    while True:
+        count += 1
+        X = Matrix([[x_old], [y_old]])
+        res: Matrix = X - W.subs({x_s: x_old, y_s: y_old}) * F.subs({x_s: x_old, y_s: y_old})
+        x, y = map(lambda x: float(x), itertools.chain.from_iterable(res.tolist()))
+        x_res.append(x)
+        y_res.append(y)
+        if e > np.sqrt((x - x_old) ** 2 + (y - y_old) ** 2):
+            break
+        x_old, y_old = x, y
+    return x_res, y_res, count
--- fourth/run.py
+++ fourth/run.py
@@ -1,16 +1,48 @@
 import numpy as np
-
+from matplotlib import pyplot as plt
+from numpy.ma import arctan
+from sympy import symbols, Matrix
 from sympy import tan
-from sympy import symbols
 
-from fourth.lib import get_yakobi
+from fourth.lib import newton_solve
 
-x, y = symbols('x y')
+plt.grid(True)
+plt.axhline(0, color='black')
+plt.axvline(0, color='black')
+plt.ylim(-1.5, 1.5)
+plt.xlim(-1.5, 1.5)
 
-functions = [
-    tan(x*y + 0.1) - x**2,
-    0.6 * x**2 + 2 * y**2 - 1
+_f1 = lambda x, y: tan(x * y + 0.1) - x ** 2
+f1 = lambda x: (arctan(x**2) - 0.1) / x
+_f2 = lambda x, y: 0.6 * x ** 2 + 2 * y ** 2 - 1
+
+x = np.linspace(-1.5, 1.5, 101)
+y = np.linspace(-1.5, 1.5, 101)
+X, Y = np.meshgrid(x, y)
+F = _f2(X, Y)
+plt.contour(X, Y, F, [0])
+plt.plot(x, f1(x))
+
+x_s, y_s = symbols('x y')
+F = Matrix([
+    [_f1(x_s, y_s)],
+    [_f2(x_s, y_s)]
+])
+
+e = 0.0001
+starts = [
+    (1.0, 0.5),
+    (0.0, -0.5),
+    (-1.0, -0.5),
+    (-0.0, 0.5),
 ]
+print('Метод Ньютона:')
+for start in starts:
+    x0, y0 = start
+    x_res, y_res, count = newton_solve('x y', (x0, y0), F, e)
+    print(f'\tДля точки {start}:')
+    print(f'\tКоличество итераций: {count}')
+    print(f'\t\tx = {x_res[-1]}; y = {y_res[-1]}')
 
-W = get_yakobi(functions, 'x y')
-print(W)
+    plt.plot(x_res, y_res, 'ro-', color='#ff0000')
+plt.savefig('fig1.png', dpi=150)
